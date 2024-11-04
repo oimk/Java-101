eb8b5748f4403fd6e68a51db5242de77f06a152d
Hiccup <jianga3@bxscience.edu>
2024-11-04 15:44:16 -0500

    Started Wokring on Reverse Arr

--- java101/Main.java
+++ java101/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.err.println(PracticeProblems.countOccurrences(new int[]{1, 2, 2, 4, 2, 2, 10}. 2));
-
+		System.out.println(PracticeProblems.countOccurrences(new int[]{1, 2, 2, 4, 2, 2, 10},  2));
+		System.out.println(PracticeProblems.Reversearr( new int[]{1,2,3,4,5,6,7,8}));
 	}
 }
--- java101/PracticeProblems.java
+++ java101/PracticeProblems.java
@@ -8,4 +8,13 @@ public class PracticeProblems {
         }
     return occurence;
     }
+    static int[] Reversearr(int[] arr){
+        int nt = arr.length;
+        int[] newarr = new int[nt];
+        for (int t = 0; t < arr.length; t++){
+            nt --; 
+            newarr[t] = arr[nt];
+        }
+    return newarr;
+    }
 }
