830cbc261818c6db9f75bbf8ef27ae7ca8df5d15
Hiccup <jianga3@bxscience.edu>
2024-11-04 15:10:31 -0500

    Save 1

--- java101/Main.java
+++ java101/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.err.println(PracticeProblems.countOccurrences(new int[]{1, 2, 2, 4, 2, 2, 10}, 2));
+		System.err.println(PracticeProblems.countOccurrences(new int[]{1, 2, 2, 4, 2, 2, 10}. 2));
 
 	}
 }
